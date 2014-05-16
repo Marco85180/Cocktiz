a941a10923a84887f9b78c6fb2fd25e46864a215
MARCO <lefollmarc@hotmail.fr>
2014-05-16 15:11:05 +0200

    add model ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516122625) do
+ActiveRecord::Schema.define(version: 20140516130806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20140516122625) do
     t.string   "title"
     t.text     "body"
     t.string   "picture_url", default: "http://upload.wikimedia.org/wikipedia/commons/e/e7/Flaming_cocktails.jpg"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.string   "picture_url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
